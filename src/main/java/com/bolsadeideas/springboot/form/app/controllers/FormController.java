eadb86979a7af714e0e39f6d7b0fdd372118908d
v4sh6n <128129211+v4sh6n@users.noreply.github.com>
2023-10-14 17:53:00 +0200

    update form

--- src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -3,6 +3,7 @@ package com.bolsadeideas.springboot.form.app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class FormController {
@@ -10,6 +11,12 @@ public class FormController {
 	@GetMapping("/form")
 	public String form(Model model) {
 		return "form";
+	}
+	
+	@PostMapping("/form")
+	public String procesar (Model model) {
+		
+		return "resultado";
 	}
 
 }
