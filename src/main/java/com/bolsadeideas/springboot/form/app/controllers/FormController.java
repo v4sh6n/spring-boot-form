ab057a9941adc80741f50fea8a23cd015b943dca
v4sh6n <128129211+v4sh6n@users.noreply.github.com>
2023-10-16 19:19:33 +0200

    validation anotation

--- src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -1,10 +1,16 @@
 package com.bolsadeideas.springboot.form.app.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class FormController {
@@ -16,15 +22,20 @@ public class FormController {
 	}
 	
 	@PostMapping("/form")
-	public String procesar (Model model, 
-			@RequestParam(name = "username") String username,
-			@RequestParam String password,
-			@RequestParam String email) {
+	public String procesar (@Valid Usuario usuario, BindingResult result, Model model){
 		
-		model.addAttribute("titulo", "Resultado form");
-		model.addAttribute("username", username);
-		model.addAttribute("password", password);
-		model.addAttribute("email", email);
+		if(result.hasErrors()) {
+			Map<String, String> errores = new HashMap<>();
+			result.getFieldErrors().forEach(err ->{
+				errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
+			});
+			model.addAttribute("error", errores);
+			return "form";
+		}
+		
+		//model.addAttribute("titulo", "Resultado form");
+		model.addAttribute("usuario", usuario);
+		
 		
 		return "resultado";
 	}
